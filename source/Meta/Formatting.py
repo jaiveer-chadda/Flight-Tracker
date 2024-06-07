542b85b66a87ff31eb7ec45d46db6312d69795d4
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-06-08 00:28:49 +0400

    Added a number of formatting functions

---
+++ source/Meta/Formatting.py
@@ -0,0 +1,43 @@
+def coordinates(latitude: float, longitude: float) -> str:
+    def convert_to_dms(decimal_degree):
+        is_positive = decimal_degree >= 0
+        decimal_degree = abs(decimal_degree)
+        degrees = int(decimal_degree)
+        minutes_full = (decimal_degree - degrees) * 60
+        minutes = int(minutes_full)
+        seconds = (minutes_full - minutes) * 60
+        if is_positive:
+            direction = 'N' if degrees in range(0, 90) else 'E'
+        else:
+            direction = 'S' if degrees in range(0, 90) else 'W'
+            
+        return (f"{degrees}°"
+                f"{str(minutes).rjust(2, "0")}'"
+                f"{str(f'{seconds:.2f}').rjust(5, '0')}\""
+                f"{direction}")
+    
+    return convert_to_dms(latitude) + " " + convert_to_dms(longitude)
+
+
+def heading(heading_: float) -> str:
+    match heading_:
+        case h if h >= 337.5 or h < 22.5:
+            direction = "N"
+        case h if 22.5 <= h < 67.5:
+            direction = "NE"
+        case h if 67.5 <= h < 112.5:
+            direction = "E"
+        case h if 112.5 <= h < 157.5:
+            direction = "SE"
+        case h if 157.5 <= h < 202.5:
+            direction = "S"
+        case h if 202.5 <= h < 247.5:
+            direction = "SW"
+        case h if 247.5 <= h < 292.5:
+            direction = "W"
+        case h if 292.5 <= h < 337.5:
+            direction = "NW"
+        case _:
+            direction = ""
+    
+    return f"{heading_}º {direction}"
--- source/main.py
+++ source/main.py
@@ -1,24 +1,29 @@
 from opensky_api import OpenSkyApi
 
+from Meta import Formatting
+
 _24_hrs_in_secs = 24 * 60 * 60
 _2_hrs_in_secs = 2 * 60 * 60
 
 api = OpenSkyApi()
 states = api.get_states()
 
-s = states.states[0]
+s = states.states[50]
+
 print(f"""
 \torigin:\t\t{s.origin_country}
-\tcall sign:\t{s.callsign if s.callsign is not None else "unknown"}
-    
-\tpos:\t\t({s.latitude}, {s.longitude})
-    
+\tcall sign:\t{"unknown" if s.callsign in [None, ""] else s.callsign}
+
+\tpos:\t\t{Formatting.coordinates(s.latitude, s.longitude)}
+
 \talt:\t\t{s.baro_altitude} m
 \tvel:\t\t{s.velocity} m/s
-\tangle:\t\t{s.true_track}º
+\theading:\t{Formatting.heading(s.true_track)}
 """)
 
-track = api.get_track_by_aircraft(states.states[0].icao24)
+track = api.get_track_by_aircraft(s.icao24)
 
-print(f"\tstart:\t\t{tuple(track.path[0][1:3])}")
-print(f"\tend:\t\t{tuple(track.path[-1][1:3])}\n")
+path_is_valid = track is not None
+
+print(f"\tstart:\t\t{Formatting.coordinates(*track.path[0][1:3]) if path_is_valid else "unknown"}")
+print(f"\tend:\t\t{Formatting.coordinates(*track.path[-1][1:3]) if path_is_valid else "unknown"}\n")
