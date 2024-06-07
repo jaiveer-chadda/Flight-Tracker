2894078becefd116d1086bbac6d67eb7362f7296
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-06-08 01:10:58 +0400

    Edited the GAD to remove all unneccesary data, and all airports with no location data

---
+++ source/Meta/ProcessGAD.py
@@ -0,0 +1,25 @@
+
+with open("../../resources/airports/GlobalAirportDatabase/GlobalAirportDatabase.txt") as file:
+    database_lines = file.read().split("\n")
+    
+list_to_upload: list[str] = []
+
+for line in database_lines:
+    if line == "":
+        continue
+    raw_data = line.split(":")
+    if raw_data[14] == "0.000" and raw_data[15] == "0.000":
+        continue
+    data = (
+        raw_data[0].upper(),
+        raw_data[2].title(),
+        raw_data[3].title(),
+        raw_data[4].title(),
+        raw_data[14],
+        raw_data[15]
+    )
+    list_to_upload.append(",".join(data))
+
+
+with open("../../resources/airports/usable_database.txt", "w") as file:
+    file.write("\n".join(list_to_upload))
